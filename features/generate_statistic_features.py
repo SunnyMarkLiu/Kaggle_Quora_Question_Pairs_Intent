4a59e5b67cf6fc4bba6c6c33fac49981dcb5fbc0
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-09-29 17:00:11 +0800

    tfidf features, local cv 0.476471

--- features/generate_statistic_features.py
+++ features/generate_statistic_features.py
@@ -13,6 +13,9 @@ import sys
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from utils import data_utils
 
 from optparse import OptionParser
@@ -25,10 +28,59 @@ def generate_contain_word_features(df, word):
     df[word + "_count_cq1"] = df['cleaned_question1'].apply(lambda x: str(x).strip().split().count(word))
     df[word + "_count_cq2"] = df['cleaned_question2'].apply(lambda x: str(x).strip().split().count(word))
 
-    df[word + "_cq1_cq2_both_gt_0"] = df.apply(lambda raw: ((raw[word + "_count_cq1"] > 0) & (raw[word + "_count_cq2"] > 0)), axis=1)
-    df[word + "_cq1_cq2_one_gt_0"] = df.apply(lambda raw: ((raw[word + "_count_cq1"] > 0) | (raw[word + "_count_cq2"] > 0)), axis=1)
-    df[word + "_cq1_cq2_diff"] = df.apply(lambda raw: (((raw[word + "_count_cq1"] > 0) & (raw[word + "_count_cq2"] <= 0)) |
+    df[word + "_cq1_cq2_both_gt_0"] = df.apply(lambda raw: int((raw[word + "_count_cq1"] > 0) & (raw[word + "_count_cq2"] > 0)), axis=1)
+    df[word + "_cq1_cq2_one_gt_0"] = df.apply(lambda raw: int((raw[word + "_count_cq1"] > 0) | (raw[word + "_count_cq2"] > 0)), axis=1)
+    df[word + "_cq1_cq2_diff"] = df.apply(lambda raw: int(((raw[word + "_count_cq1"] > 0) & (raw[word + "_count_cq2"] <= 0)) |
                                                       ((raw[word + "_count_cq1"] <= 0) & (raw[word + "_count_cq2"] > 0))), axis=1)
+
+
+def generate_matchshared_words_features(row, question):
+    """
+    两个句子中都存在的词汇总数
+    """
+    q1words = {}
+    q2words = {}
+    for word in str(row[question+"1"]).lower().split():
+        q1words[word] = q1words.get(word, 0) + 1
+    for word in str(row[question+"2"]).lower().split():
+        q2words[word] = q2words.get(word, 0) + 1
+    n_shared_word_in_q1 = sum([q1words[w] for w in q1words if w in q2words])
+    n_shared_word_in_q2 = sum([q2words[w] for w in q2words if w in q1words])
+    n_tol = sum(q1words.values()) + sum(q2words.values())
+    if 1e-6 > n_tol:
+        return 0.
+    else:
+        return 1.0 * (n_shared_word_in_q1 + n_shared_word_in_q2) / n_tol
+
+
+def train_tfidf_vectorizer(train, test, question):
+    tfidf = TfidfVectorizer(ngram_range=(1, 1))
+    tfidf_txt = pd.Series(
+                train[question+'1'].tolist() + train[question+'2'].tolist() +
+                test[question+'1'].tolist() + test[question+'2'].tolist()).astype(str)
+    tfidf.fit(tfidf_txt)
+    return tfidf
+
+
+def generate_tfidf_features(tfidf_vectorizer, df, question):
+    """
+    TFIDF相关统计特征
+    """
+    def calc_tfidf(raw):
+        cq1_tfidf = np.sum(tfidf_vectorizer.transform([str(raw[question+'1'])]).data)
+        cq2_tfidf = np.sum(tfidf_vectorizer.transform([str(raw[question+'2'])]).data)
+        return cq1_tfidf, cq2_tfidf
+
+    df['tfidf'] = df.apply(lambda raw: calc_tfidf(raw), axis=1)
+
+    df['sum_'+question+'1_tfidf'] = df['tfidf'].map(lambda raw: np.sum(raw[0]))
+    df['sum_'+question+'2_tfidf'] = df['tfidf'].map(lambda raw: np.sum(raw[1]))
+    df['mean_'+question+'1_tfidf'] = df['tfidf'].map(lambda raw: np.mean(raw[0]))
+    df['mean_'+question+'2_tfidf'] = df['tfidf'].map(lambda raw: np.mean(raw[1]))
+
+    df.drop(['tfidf'], inplace=True, axis=1)
+    return df
+
 
 def main(base_data_dir):
     op_scope = 1
@@ -39,11 +91,29 @@ def main(base_data_dir):
     train, test = data_utils.load_dataset(base_data_dir, op_scope)
     print("train: {}, test: {}".format(train.shape, test.shape))
 
-    print('---> generate contain_word count features')
+    print('---> generate contain word count features')
     generate_contain_word_features(train, 'not')
     generate_contain_word_features(test, 'not')
     generate_contain_word_features(train, 'best')
     generate_contain_word_features(test, 'best')
+
+    print('---> generate match shared words features')
+    train['q_match_shared_words'] = train.apply(lambda x: generate_matchshared_words_features(x, "question"), axis=1)
+    test['q_match_shared_words'] = test.apply(lambda x: generate_matchshared_words_features(x, "question"), axis=1)
+    train['cq_match_shared_words'] = train.apply(lambda x: generate_matchshared_words_features(x, "cleaned_question"), axis=1)
+    test['cq_match_shared_words'] = test.apply(lambda x: generate_matchshared_words_features(x, "cleaned_question"), axis=1)
+
+    print('---> calc cleaned_question tfidf object')
+    tfidf_vectorizer = train_tfidf_vectorizer(train, test, question='cleaned_question')
+    print('---> generate cleaned_question tfidf features')
+    train = generate_tfidf_features(tfidf_vectorizer, train, question='cleaned_question')
+    test = generate_tfidf_features(tfidf_vectorizer, test, question='cleaned_question')
+
+    print('---> calc question tfidf object')
+    tfidf_vectorizer = train_tfidf_vectorizer(train, test, question='question')
+    print('---> generate question tfidf features')
+    train = generate_tfidf_features(tfidf_vectorizer, train, question='question')
+    test = generate_tfidf_features(tfidf_vectorizer, test, question='question')
 
     print("train: {}, test: {}".format(train.shape, test.shape))
     print("---> save datasets")
